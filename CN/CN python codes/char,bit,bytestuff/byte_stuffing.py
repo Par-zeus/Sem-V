85d963fcb82e5cf0ab48f1d4afa26b83ab96514c
Par-zeus <143513965+Par-zeus@users.noreply.github.com>
2024-11-24 12:27:26 +0530

    Update byte_stuffing.py

--- CN/CN python codes/char,bit,bytestuff/byte_stuffing.py
+++ CN/CN python codes/char,bit,bytestuff/byte_stuffing.py
@@ -1,14 +1,18 @@
-# ip=goodday, d,g,$
+# Byte Stuffing
 
-main_string = input('Enter the main string:') 
-start = input('Enter the start character:') 
-endd = input('Enter the end character:') 
-delimiter = input('Enter the delimiter:') 
- 
-stuffed_string = f'{start}' 
-for i in main_string: 
-    if i==start or i==endd: 
-        stuffed_string+=f'{delimiter}' 
-    stuffed_string+=i 
-stuffed_string+=f'{endd}' 
-print(stuffed_string) 
+def byte_stuffing(data,flag='FLAG',esc='ESC'):
+    stuffed_data=data.replace(esc,esc+esc)
+    stuffed_data=stuffed_data.replace(flag,esc+flag)
+    return f"{flag}{stuffed_data}{flag}"
+
+def byte_unstuffing(frame,flag='FLAG',esc='ESC'):
+    stuffed_data=frame[len(flag):-len(flag)]
+    unstuffed_data=stuffed_data.replace(esc+flag,flag)
+    unstuffed_data=unstuffed_data.replace(esc+esc,esc)
+    return unstuffed_data
+
+data="ESC FLAG World"
+x=byte_stuffing(data)
+print("Frame:",x)
+y=byte_unstuffing(x)
+print("Deframe:",y)
